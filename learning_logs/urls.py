bce05fc1f585bd3736fada71c1abf4b731dd918e
esfekia <sertac@gmail.com>
2022-01-25 05:51:41 -0500

    Added forms.py for user input, updated urls.py to include new_topic section of the page.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,10 +5,12 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-	#Home page
-	path('', views.index, name='index'),
-	#Page that shows all topics.
-	path('topics/', views.topics, name='topics'),
-	#Details page for a single topic.
-	path('topics/<int:topic_id>/', views.topic, name='topic'),
-	]
+    # Home page
+    path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Details page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic.
+    path('new_topic/', views.new_topic, name='new_topic'),
+]
